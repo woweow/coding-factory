b829b7e4103c6525d5c6b1fc119ebb1a0a8b6cbc
eow <16455932+woweow@users.noreply.github.com>
2026-08-17 21:34:57 -0700

    only start works

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import * as readline from 'node:readline/promises';
 import { Effect, Schema } from "effect"
 
 const State = Schema.TaggedUnion({
-  Idle: {},
+  Idle: { count: Schema.Number },
   Running: { count: Schema.Number }
 })
 
@@ -11,6 +11,7 @@ const States = Machine.states(State.cases)
 const CounterEvent = Machine.events(
   Schema.TaggedUnion({
     Start: {},
+    Status: {},
     Increment: {},
     Stop: {}
   })
@@ -22,7 +23,7 @@ const CounterDefinition = Machine.make({
   events: CounterEvent,
   initial: {
     target: (to) => to.Idle(),
-    resolve: ({ target }) => target.from()
+    resolve: ({ target }) => target.from({ count: 0 })
   }
 })
 
@@ -35,8 +36,15 @@ const Counter = CounterDefinition.handle({
           console.log('Starting machine...')
           return target.from({ count: 0 })
         }
+      }),
+      Status: Machine.transition({
+        target: (to) => to.full.Running(),
+        resolve: ({ state, target }) => {
+          console.log(`State: ${state.count}`)
+          return target.from({ count: state.count })
+        }
       })
-    }
+    },
   },
   Running: {
     on: {
@@ -49,9 +57,9 @@ const Counter = CounterDefinition.handle({
       }),
       Stop: Machine.transition({
         target: (to) => to.full.Idle(),
-        resolve: ({ target }) => {
+        resolve: ({ state, target }) => {
           console.log('stopping')
-          return target.from()
+          return target.from({ count: state.count })
         }
       })
     }
@@ -63,24 +71,47 @@ const increment = Effect.gen(function* () {
   yield* ref.send(CounterEvent.Increment())
 })
 
-
 const stop = Effect.gen(function* () {
   const ref = yield* Machine.start(Counter)
   yield* ref.send(CounterEvent.Stop())
 })
 
-const start = Effect.gen(function* () {
+const status = Effect.gen(function* () {
   const ref = yield* Machine.start(Counter)
   yield* ref.send(CounterEvent.Start())
 })
 
+const start = Effect.gen(function* () {
+  const ref = yield* Machine.start(Counter)
+  yield* ref.send(CounterEvent.Start())
+  console.log('started')
+})
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
 while (true) {
+  console.log('awaiting input...')
   const input = await rl.question('> ');
-  // input variable contains the terminal input
-  console.log(input)
+  switch (input) {
+    case 'exit':
+      break;
+    case 'inc':
+      await Effect.runPromise(increment)
+      break;
+    case 'stop':
+      await Effect.runPromise(stop)
+      break;
+    case 'start':
+      await Effect.runPromise(start)
+      break;
+    case 'status':
+      await Effect.runPromise(status)
+
+      break;
+    default:
+      console.log('invalid arg')
+      break;
+  }
 }
